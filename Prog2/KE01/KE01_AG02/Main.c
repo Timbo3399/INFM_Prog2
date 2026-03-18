311e31f52443aa5937f959bd3bc2ba46079fbac6
Timbo3399 <timolahres@hotmail.com>
2026-03-18 16:46:25 +0100

    AG02

--- Prog2/KE01/KE01_AG02/Main.c
+++ Prog2/KE01/KE01_AG02/Main.c
@@ -2,6 +2,17 @@
 
 void main()
 {
-	printf("Sizeof char in Bytes ist: %u\n", sizeof(char));
-
+	printf("Sizeof char in Bytes ist: %zu\n", sizeof(char));
+	printf("Sizeof signed char in Bytes ist: %zu\n", sizeof(signed char));
+	printf("Sizeof unsigned char in Bytes ist: %zu\n", sizeof(unsigned char));
+	printf("Sizeof short int in Bytes ist: %zu\n", sizeof(short int));
+	printf("Sizeof int in Bytes ist: %zu\n", sizeof(int));
+	printf("Sizeof unsigned short int in Bytes ist: %zu\n", sizeof(unsigned short int));
+	printf("Sizeof unsigned int in Bytes ist: %zu\n", sizeof(unsigned int));
+	printf("Sizeof long int in Bytes ist: %zu\n", sizeof(long int));
+	printf("Sizeof unsigned long int in Bytes ist: %zu\n", sizeof(unsigned long int));
+	printf("Sizeof long long int in Bytes ist: %zu\n", sizeof(long long int));
+	printf("Sizeof unsigned long long int in Bytes ist: %zu\n", sizeof(unsigned long long int));
+	printf("Sizeof float in Bytes ist: %zu\n", sizeof(float));
+	printf("Sizeof double in Bytes ist: %zu\n", sizeof(double));
 }
