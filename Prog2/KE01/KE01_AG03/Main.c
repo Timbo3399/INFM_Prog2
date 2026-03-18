86169b18f86d9cbb29c82f176bfc524df02201f9
Timbo3399 <timolahres@hotmail.com>
2026-03-18 17:00:45 +0100

    AG03

--- Prog2/KE01/KE01_AG03/Main.c
+++ Prog2/KE01/KE01_AG03/Main.c
@@ -3,7 +3,35 @@
 void main()
 {
 	char cVal = 'x';
+	short int siVal = 1;
+	int iVal = 123456;
+	float fVal = 1.23f;
+	double dVal = 3.14156;
 
-	printf("%c", cVal);
+	printf("%c\n", cVal);
+	printf("%i\n", siVal);
+	printf("%i\n", iVal);
+    printf("%.2f\n", fVal);
+	printf("%f\n", dVal);
+
+	const int ciVal = 5;
+
+	printf("%i\n", ciVal);
+
+	/*
+	Was passiert beim Compilieren, wenn Sie bei der Definition der konstanten Variablen dieser keinen Wert zuweisen?
+	
+
+	Warning: Local Variable 'ciVal' is uninitialized
+	
+	*/
+
+	/*
+	Ist es möglich danach den Wert zu ändern? 
+
+
+	Nein, da ciVal als const deklariert ist, kann sein Wert nach der Initialisierung nicht mehr geändert werden. Ein Versuch, den Wert zu ändern, würde zu einem Kompilierungsfehler führen.
+
+	*/
 
 }
