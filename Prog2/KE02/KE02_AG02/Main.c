bdd7f4f522f9f8746aef22b2f9f2c4e59cbdf292
Timbo3399 <timolahres@hotmail.com>
2026-03-18 17:58:16 +0100

    AG02

--- Prog2/KE02/KE02_AG02/Main.c
+++ Prog2/KE02/KE02_AG02/Main.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
+#include <math.h>
+
+double calculateVolume(double dR);
 
 void main()
 {
-	double dR;
-	scanf("%lf", &dR);
+	double dR = 1.5;
 	printf("Das Volumen der Kugel mit Radius %f ist: %f\n", dR, calculateVolume(dR));
 }
 
 double calculateVolume(double dR)
 {
 	const double dPi = 3.14159;
-	return (4.0 / 3.0) * dPi * pow(dR,3);
+	double dVolume = (4.0 / 3.0) * dPi * pow(dR, 3.0);
+	return dVolume;
 }
+
+/*
+mit #include <math.h>
+-1 ergebt -4.188787
+0 ergebt 0
+1 ergebt 4.188787
+1.5 ergebt 14.137155
+
+ohne #include <math.h>
+-1 ergebt -64808.907307 
+0 ergebt 0 
+1 ergebt -64808.907307 
+1.5 ergebt 6500.996907
+Da die Funktion pow() ohne die Einbindung der math.h-Bibliothek nicht definiert ist und deshalb zu einem undefinierten Verhalten führt.
+*/
