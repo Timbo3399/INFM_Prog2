3de44d28616cdeb706a59e2cbd8ea5d7945de7b5
TIMO-LAPTOP\timol <timolahres@hotmail.com>
2026-03-23 16:10:12 +0100

    ag03 finished

--- Prog2/KE03/KE03_AG03/Main.c
+++ Prog2/KE03/KE03_AG03/Main.c
@@ -4,16 +4,23 @@
 
 main(void)
 {
+	printf("Input iVar:");
 	int iVar = _getch();
-	printf("iVar %d\n", iVar);
-	printf("iVar as ASCII = %c\n", iVar);
+	printf("\n");
+	printf("iVar %c\n", iVar);
+	printf("iVar as ASCII = %d\n", iVar);
 
+	printf("Input iVar2:");
 	int iVar2 = _getch();
+	printf("\n");
 
 	_clrscr();
 
-	_gotoxy(iVar, iVar2);
+	printf("GoTo X: %i Y: %i",iVar2 % 10,iVar2 % 10);
+
+	_gotoxy(iVar2 % 10, iVar2 % 10);
 
 
 
 }
+
