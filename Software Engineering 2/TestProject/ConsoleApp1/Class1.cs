45cbe6cf1f30a0a3b66007909ddde2b061a4321b
TIMO-LAPTOP\timol <timolahres@hotmail.com>
2026-03-27 17:17:25 +0100

    console test strings

---
+++ Software Engineering 2/TestProject/ConsoleApp1/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        internal void doIt()
+        {
+            string s1 = "hello";
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for(int i = 0; i<100000; i++)
+            {
+                s1 += "AI";
+            }
+            stopwatch.Stop();
+            stopwatch.Reset();
+
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+            var sb = new StringBuilder();
+
+            sb.Append("hello");
+
+            stopwatch.Start();
+            for (int i = 0; i < 100000; i++)
+            {
+                sb.Append("AI");
+            }
+
+            string a = sb.ToString();
+
+            stopwatch.Stop();
+
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+        }
+    }
+
+    
+
+
+}
---
+++ Software Engineering 2/TestProject/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new Class1();
+
+            test.doIt();
+
+            Console.ReadLine();
+        }
+    }
+}
