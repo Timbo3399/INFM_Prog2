4089ce90b816e1dfc26df87f37d7ee0250ad26e5
Timbo3399 <timolahres@hotmail.com>
2026-03-29 13:45:48 +0200

    aufgabe 3 fertig

--- Prog2/Uebung_KE04/KE04_AG03/Main.c
+++ Prog2/Uebung_KE04/KE04_AG03/Main.c
@@ -1,6 +1,58 @@
 #include <stdio.h>
 
-int mian (void)
+int main (void)
 {
+	int iValh;
+	int iValb;
+
+	printf("Hoehe: ");
+	scanf_s("%i", &iValh);
+
+	printf("\nBreite: ");
+	scanf_s("%i", &iValb);
+
+	if (iValh % 2 == 0 || iValb %2 == 0)
+	{
+		printf("Fehlerhafte Eingabe Hoehe: %i oder Breite: %i ist gerade", iValh, iValb);
+		return 0;
+	}
+
+
+	printf("Figur 1\n");
+	/*	Figur 1
+		ABBBB
+		AABBB
+		AAABB
+		AAAAB
+		AAAAA 
+	 */
+	for (int i = 0; i < iValh; i++)
+	{
+		for (int j = 0; j <iValb; j++ )
+		{
+			printf("%c", (j <= i) ? 'A' : 'B');
+		}
+		printf("\n");
+	}
+
+	/*	Figur 2
+		ABBBB
+		AABBB
+		AAABB
+		AABBB
+		ABBBB 
+	 */
+
+	printf("Figur 2\n");
+
+	for (int i = 0; i < iValh; i++)
+	{
+		for (int j = 0; j < iValb; j++)
+		{
+			printf("%c", (j <= (i <= iValh / 2 ? i : iValh - 1 - i)) ? 'A' : 'B');
+		}
+		printf("\n");
+
+	}
 	return 0;
 }
