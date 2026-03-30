b08cc8e12fbcb2dcbbdf82a3f0bb1e27ea741ad3
TIMO-LAPTOP\timol <timolahres@hotmail.com>
2026-03-30 16:36:58 +0200

    aufgabe 6 fertig

---
+++ Prog2/Uebung_KE05/KE05_AG06/Main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define ARRAY_LEN (3)
+
+struct Employee
+{
+	char cFirstName[15];
+	char cLastName[15];
+	unsigned int uiAge;
+};
+
+int main(void)
+{
+	struct Employee s_employee[ARRAY_LEN]= 
+	{
+		{"Timo","Lahres",25},
+		{"TestFirst","TestLast",99},
+		{"Test2First","Test2Last",42}
+	}; 
+
+	for (int i = 0; i<ARRAY_LEN;i++)
+	{
+		printf("%i: FirstName",i);
+		scanf_s("%14s", s_employee[i].cFirstName, sizeof(s_employee[i].cFirstName));
+		printf("%i: LastName", i);
+		scanf_s("%14s", s_employee[i].cLastName, sizeof(s_employee[i].cLastName));
+		printf("%i: Age", i);
+		scanf_s("%u", &s_employee[i].uiAge);
+	}
+
+	for (int i = 0; i<ARRAY_LEN; i++)
+	{
+		printf("FirstName: %s LastName: %s Age: %u\n", s_employee[i].cFirstName, s_employee[i].cLastName, s_employee[i].uiAge);
+	}
+	return 0;
+}
